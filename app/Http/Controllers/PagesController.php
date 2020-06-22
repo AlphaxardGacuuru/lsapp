02004333e48d1c5a630d9d65934f64051015fb34
ALphaxard <alphaxardgacuuru47@gmail.com>
2020-06-22 18:28:53 +0300

    init

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        $title = 'Welcome to Laravel!';
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+
+    }
+
+    public function about()
+    {
+        $title = 'About us.';
+        return view('pages.about')->with('title', $title);
+    }
+
+    public function services()
+    {
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO'],
+        );
+        return view('pages.services')->with($data);
+    }
+}
